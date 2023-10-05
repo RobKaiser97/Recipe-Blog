dc0d92d968430c7c9f8eba18c2eb421912411f57
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-10-05 19:48:24 -0400

    Copying html info into handlebars css and js

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+const categoriesDropdown = document.getElementById('categoriesDropdown');
+const profileDropdown = document.getElementById('profileDropdown');
+
+document.querySelector('.group:nth-child(2)').addEventListener('mouseenter', () => {
+    categoriesDropdown.classList.remove('hidden');
+});
+
+document.querySelector('.group:nth-child(2)').addEventListener('mouseleave', () => {
+    categoriesDropdown.classList.add('hidden');
+});
+
+document.querySelector('.group:nth-child(3)').addEventListener('mouseenter', () => {
+    profileDropdown.classList.remove('hidden');
+});
+
+document.querySelector('.group:nth-child(3)').addEventListener('mouseleave', () => {
+    profileDropdown.classList.add('hidden');
+});
