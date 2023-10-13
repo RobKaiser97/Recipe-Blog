7e3974f3c9c9fd9dcb2b231890851b300f13c0e2
jp0st <jpost2010@gmail.com>
2023-10-12 22:39:05 -0400

    functionality for comments adding

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -13,6 +13,7 @@ router.post('/', withAuth, async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+  console.log("this is req body", req.body);
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
